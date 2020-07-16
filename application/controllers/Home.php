1571b1c58e728b350de0ed1b55e8c96f1a87847a
almaprayuda <allma.prayuddha@gmail.com>
2020-07-16 20:05:19 +0700

    new commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,7 +10,7 @@ class Home extends CI_Controller {
 
 		if ($this->form_validation->run() === FALSE) {
 			$this->load->view('login');
-		} else {
+} else {
 			$email = htmlspecialchars($this->input->post('email'));
 			$password = $this->input->post('password');
 
